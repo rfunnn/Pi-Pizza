99031d0e5f317c9678cf9637fc10664027ac5793
Arfan <arfan.gt@gmail.com>
2022-07-16 13:46:35 +0800

    Files upload

---
+++ RazorPizza/Controllers/YoutubeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RazorPizza.Controllers
+{
+    public class YoutubeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
